e7a4d5cecd221c6d07643d676578ef04c6d84962
longfeizheng <longfei_zheng@163.com>
2017-12-11 21:42:27 +0800

    Initial commit

---
+++ src/main/java/cn/merryyou/chapter03_8/StaticDispatch.java
@@ -0,0 +1,44 @@
+package cn.merryyou.chapter03_8;
+
+/**
+ * 方法静态分派演示
+ * Created on 2017/12/11 0011.
+ *
+ * @author zlf
+ * @email i@merryyou.cn
+ * @since 1.0
+ */
+public class StaticDispatch {
+
+    public static void main(String[] args) {
+        Human man = new Man();
+        Human woman = new Woman();
+        StaticDispatch sr = new StaticDispatch();
+        sr.sayHello(man);
+        sr.sayHello(woman);
+    }
+
+    public void sayHello(Human guy) {
+        System.out.println("hello guy!");
+    }
+
+    public void sayHello(Man guy) {
+        System.out.println("hello gentleman!");
+    }
+
+    public void sayHello(Woman guy) {
+        System.out.println("hello lady");
+    }
+
+    static abstract class Human {
+
+    }
+
+    static class Man extends Human {
+
+    }
+
+    static class Woman extends Human {
+
+    }
+}
---
+++ src/main/java/cn/merryyou/chapter04_10/AutoBoxingTest.java
@@ -0,0 +1,44 @@
+package cn.merryyou.chapter04_10;
+
+/**
+ * 自动装箱的陷阱  IntegerCache - 128 - 127
+ * Created on 2017/12/11 0011.
+ *
+ * @author zlf
+ * @email i@merryyou.cn
+ * @since 1.0
+ */
+public class AutoBoxingTest {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3l;
+        Long h = 4l;
+        Long i = 7l;
+        Long j = 321l;
+        Long k = 321l;
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        /** 源码显示
+         public boolean equals(Object obj) {
+         if (obj instanceof Integer) {
+         return value == ((Integer)obj).intValue();
+         }
+         return false;
+         }
+         */
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
+        System.out.println(i == (g + h));
+        System.out.println(i.equals(g + h));
+        System.out.println(c.equals(g));
+        System.out.println(k == j);
+        System.out.println(k.equals(j));
+    }
+}
